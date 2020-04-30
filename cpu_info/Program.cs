cf1f98c390ca466d77e629c443ef268ee89f6522
share234 <apsystems.sh@gmail.com>
2020-04-30 17:47:22 +0100

    started

---
+++ cpu_info/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace cpu_info
+{
+    class getcpudata
+    {
+        float[,] thread_array;
+        public getcpudata(int threadcount)
+        {
+            thread_array = new float[threadcount, 2];
+        }
+    }
+
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
